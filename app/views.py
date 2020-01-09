c3b6e869a7a92a1a428384a068ec2d61f36b85d3
Craig <craig.attwood@hotmail.co.uk>
2020-01-09 22:28:46 +0000

    missed view.py's refs

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import (
     Blueprint, jsonify, flash, g, redirect, render_template, request, session, url_for
 )
 import datetime
-from auth import login_required
+from .auth import login_required
 
 bp = Blueprint('page', __name__, url_prefix='/')
 
